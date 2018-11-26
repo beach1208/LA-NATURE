c4ed5e77aabc8accc8dc9c169b264177b8df324b
Nagisa Kojima <nk.bg.1208@gmail.com>
2018-11-25 16:08:28 -0800

    basic structure

---
+++ footer.php
@@ -0,0 +1,9 @@
+<footer>
+  <div class="footer-inner">
+    footer
+ 
+  </div>
+</footer>
+<?php wp_footer(); ?><!--システム・プラグイン用-->
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?> ">
+    
+<?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+    <header>
+        <div class="header-inner">
+   header
+</div>
+</header>
