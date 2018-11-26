f9bbea4e4abd82a80766b7aed72d674de0f186ba
Nagisa Kojima <nk.bg.1208@gmail.com>
2018-11-26 10:06:05 -0800

    layout organized

--- header.php
+++ header.php
@@ -6,12 +6,21 @@
     <title>Page Title</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?> ">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
     
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
     <header>
         <div class="header-inner">
+        <div class="social-icons">
+                <ul>
+                    <li>instagram</li>
+                    <li>linkedin</li>
+                    <li>dribble</li>
+                </ul>
+            </div>
+          
             <div class="site-title">
                 <h1><a href="<?php echo home_url(); ?>">
                 <?php bloginfo('name'); ?>
@@ -19,7 +28,7 @@
             </div>
             <!--mobile button-->
 <button type="button" id="navbutton">
- <i class="fas fa-list-ul"></i><
+ <i class="fas fa-list-ul"></i>
 </button>
 
 </div>
