0a664ac42d5af1b33be9739a6fc9f88bacd8a55f
Nagisa Kojima <nk.bg.1208@gmail.com>
2018-12-01 20:01:50 -0800

    sigle article  added

--- single.php
+++ single.php
@@ -2,6 +2,42 @@
 <div class="container">
     <div class="contents">
 
+<!-- loop for single article -->
+
+<?php if (have_posts()) : the_post(); ?>
+
+<article <?php post_class('article'); ?>>
+
+<!-- article contents -->
+
+<div class="article-info">
+
+<!-- day of post -->
+
+<span class="article-date">
+<i class="fas fa-pencil-alt"></i>
+    <time
+    datetime="<?php echo get_the_date('Y-m-d'); ?>">
+    <?php echo get_the_date(); ?>
+    </time>
+</span>
+
+<!-- category get -->
+
+<?php if (has_category()) : ?>
+<span class="cat-data">
+    
+    <?php echo get_the_category_list(' '); ?>
+</span>
+<?php endif; ?>
+</div>
+
+</article>
+
+<?php endif; ?>
+
+
+
     </div>
     <?php get_sidebar(); ?>
 </div>
